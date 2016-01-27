93d955c0e8d207a5d0ca74b7c59eff7a789e5fbd
Patrick Goley <patrick.goley@gmail.com>
2016-01-27 01:28:20 -0600

    initial commit

---
+++ fastlanetodo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fastlanetodo
+//
+//  Created by Patrick Goley on 1/27/16.
+//  Copyright © 2016 todo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ fastlanetodo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fastlanetodo
+//
+//  Created by Patrick Goley on 1/27/16.
+//  Copyright © 2016 todo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
